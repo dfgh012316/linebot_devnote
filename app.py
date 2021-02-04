fe789281b0c4c9e0fb0134ddcd64aa4233bc1681
Jerry Chuang <dfgh012316@gmail.com>
2021-02-04 11:08:19 +0800

    v4
    1.新增好友訊息
    2.圖表顯示中文

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort,render_template
+import matplotlib.pyplot as plt
 import os,sys,sqlite3,json
 from dotenv import load_dotenv
 from linebot import (
@@ -11,7 +12,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,StickerSendMessage,PostbackEvent,
     TemplateSendMessage,ButtonsTemplate,MessageTemplateAction,FlexSendMessage
 )
-from models.plot import return_message,picture,judge,return_pass_subject,create_data
+from models.plot import return_message,picture,judge,return_pass_subject,create_data,search_ID_DICT
 import pandas as pd
 app = Flask(__name__)
 
@@ -22,9 +23,12 @@ channel_access_token=os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+plt.switch_backend('agg') #不需要圖形介面的的backend
+plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] #顯示中文字
+
 #讀取成績單及通過標準
 data=create_data()
-standar={'memory':80,'understand':70,'application':60,'analyse':60,'judge':60,'cool':60}
+standar={'知識_40%':80,'能力_40%':70,'態度_20%':60}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -44,35 +48,42 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try :
         reply_arr=[]                           #回覆的訊息list
-        input_data=event.message.text.lower()  #大小寫都可搜尋
-        content=return_message(data,input_data) #回覆成績資料
-        grade_picture=picture(standar,data,input_data)  #生成圖表及回傳URL
-        pass_subject=judge(standar,data,input_data)  #產生通過的科目List
-        content2=return_pass_subject(pass_subject)
-        reply_arr.append(TextSendMessage(content))  #將文字訊系放入reply陣列
-        reply_arr.append(ImageSendMessage(grade_picture,grade_picture)) #將圖表url放入回傳陣列
-        if pass_subject :
-            reply_arr.append(TextSendMessage(content2))
-            reply_arr.append(StickerSendMessage(
-                package_id='11537',
-                sticker_id='52002735')
+        input_data=event.message.text.upper()  #大小寫都可搜尋
+        if len(search_ID_DICT(data,input_data)['ID']) != 0 :
+            content=return_message(data,input_data) #回覆成績資料
+            grade_picture=picture(standar,data,input_data)  #生成圖表及回傳URL
+            pass_subject=judge(standar,data,input_data)  #產生通過的科目List
+            content2=return_pass_subject(pass_subject)
+            reply_arr.append(TextSendMessage(content))  #將文字訊系放入reply陣列
+            reply_arr.append(ImageSendMessage(grade_picture,grade_picture)) #將圖表url放入回傳陣列
+            if pass_subject :
+                reply_arr.append(TextSendMessage(content2))
+                reply_arr.append(StickerSendMessage(
+                    package_id='11537',
+                    sticker_id='52002735')
                 )
-        line_bot_api.reply_message(
+            line_bot_api.reply_message(
                 event.reply_token,reply_arr)
+        else :
+            line_bot_api.reply_message(
+            event.reply_token,TextSendMessage("查無此學號，請重新輸入。")
+        )
+
     except :
         line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("查無此學號")
+            event.reply_token,TextSendMessage("LineBot怪怪的請聯絡老師或助教")
         )
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
     if event.postback.data == '查詢成績' :
         line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("請輸入您的學號")
+            event.reply_token,TextSendMessage("請輸入您的學號+安全碼(兩碼)")
         )
     elif event.postback.data == '重要公告' :
         line_bot_api.reply_message(
--- models/plot.py
+++ models/plot.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as  pd
 import numpy as np
 
-#test data
-# data_from_excel=pd.read_excel('測試成績單.xlsx',sheet_name=None,engine='openpyxl')
-# print(data_from_excel)
 def create_data():
     df = pd.DataFrame()
  
@@ -15,8 +12,6 @@ def create_data():
     for s_name in sheet.sheet_names:
         df = pd.concat([df, data.get(s_name)], ignore_index=False)
     return df
-plt.switch_backend('agg') #不需要圖形介面的的backend
-#plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] 顯示中文字
 
 def search_ID_DICT(data,ID):
     try:
@@ -28,8 +23,8 @@ def search_ID_DICT(data,ID):
 
 def judge(standar,data,ID):
     df=data.loc[data['ID']==ID].fillna(0)
-    grade=[int(df[i]) for i in df.columns[2:8]]      #成績  
-    subject=[i for i in df.columns[2:8]]             #科目
+    grade=[int(df[i]) for i in df.columns[2:5]]      #成績  
+    subject=[i for i in df.columns[2:5]]             #科目
     student_dict=dict(zip(subject,grade))
     pass_subject=[]
     for i in subject :
@@ -42,7 +37,7 @@ def plot_judge(standar,data,ID):
     pass_list=[]
     pass_subject_list=judge(standar,data,ID)
 
-    subjects=[i for i in df.columns[2:8]]
+    subjects=[i for i in df.columns[2:5]]
 
     for subject in subjects :
         if subject not in pass_subject_list :
@@ -53,47 +48,44 @@ def plot_judge(standar,data,ID):
     return pass_list
 
 def return_message(data,input_data):
-    if search_ID_DICT(data,input_data) == False :
-        return "查無此學號，請重新輸入"
+    if len(search_ID_DICT(data,input_data)['ID']) == 0 :
+        return 
     else :
-        content='記憶:'+str(search_ID_DICT(data,input_data)['memory']).replace('[','').replace(']','')+'\n'+\
-                '理解:'+str(search_ID_DICT(data,input_data)['understand']).replace('[','').replace(']','')+'\n'+\
-                '應用:'+str(search_ID_DICT(data,input_data)['application']).replace('[','').replace(']','')+'\n'+\
-                '分析:'+str(search_ID_DICT(data,input_data)['analyse']).replace('[','').replace(']','')+'\n'+\
-                '評鑑:'+str(search_ID_DICT(data,input_data)['judge']).replace('[','').replace(']','')+'\n'+\
-                '創意:'+str(search_ID_DICT(data,input_data)['cool']).replace('[','').replace(']','')
+        content='知識:'+str(search_ID_DICT(data,input_data)['知識_40%']).replace('[','').replace(']','')+'\n'+\
+                '能力:'+str(search_ID_DICT(data,input_data)['能力_40%']).replace('[','').replace(']','')+'\n'+\
+                '態度:'+str(search_ID_DICT(data,input_data)['態度_20%']).replace('[','').replace(']','')
+               
 
         return content
 
 
 def picture(standar,data,ID):
-    df=data.loc[data['ID']==ID].fillna(0)            #處理尚未填入成績的空欄位
-    grade=[int(df[i]) for i in df.columns[2:8]]      #成績  
-    subject=[i for i in df.columns[2:8]]             #科目
-
-    '''
-    [2:8]為截取成績單中的第2欄到第7欄中的科目
-    '''
-    pass_list=plot_judge(standar,data,ID)
-    subject,flag=zip(*pass_list)
-    x=np.arange(len(subject))                       #取得科目數量
-    bars=plt.bar(x,grade,tick_label=subject,color='blue')  
+    if len(search_ID_DICT(data,ID)['ID']) == 0 :
+        return "查無此學號，請重新輸入。"
+    else :
+        df=data.loc[data['ID']==ID].fillna(0)            #處理尚未填入成績的空欄位
+        grade=[int(df[i]) for i in df.columns[2:5]]      #成績  
+        subject=[i for i in df.columns[2:5]]             #科目
+        pass_list=plot_judge(standar,data,ID)
+        subject,flag=zip(*pass_list)
+        x=np.arange(len(subject))                       #取得科目數量
+        bars=plt.bar(x,grade,tick_label=subject,color='blue')  
     
     
-    for a,b in zip([i for i in range(6)],df.columns[2:8]):
-        plt.text(a,int(df[b]),int(df[b]),size=18,horizontalalignment='center')
+        for a,b in zip([i for i in range(6)],df.columns[2:5]):
+            plt.text(a,int(df[b]),int(df[b]),size=18,horizontalalignment='center')
 
-    for i, bar in enumerate(bars): 
-        if flag[i] == 0 : 
-            bar.set_color("red")
+        for i, bar in enumerate(bars): 
+            if flag[i] == 0 : 
+                bar.set_color("red")
 
-    plt.xlabel('Subject')
-    plt.ylabel('Score')
-    plt.title(ID)
-    plt.ylim(0,100)
-    plt.savefig('static//{}.png'.format(ID))
-    plt.close()
-    return 'https://32fd4f5f25ee.ngrok.io//static//{}.png'.format(ID)  #mac上改用 /  win上用//
+        plt.xlabel('三大領域')
+        plt.ylabel('成績')
+        plt.title(ID)
+        plt.ylim(0,100)
+        plt.savefig('static//{}.png'.format(ID))
+        plt.close()
+        return 'https://601dbe51ee90.ngrok.io//static//{}.png'.format(ID)  #mac上改用 /  win上用//
 
 def return_pass_subject(pass_subject):
     content="恭喜"
@@ -102,16 +94,17 @@ def return_pass_subject(pass_subject):
             content+=j
         else :
             content=content+j+','
-    return content+'通過'
+    return (content+'通過').replace("_40%","").replace("_20%","")
 
 
 if __name__ == '__main__':
     plt.switch_backend('agg') #不需要圖形介面的的backend
-    #plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] 顯示中文字
+    plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] #顯示中文字
     data=create_data()
-    standar={'memory':80,'understand':70,'application':60,'analyse':60,'judge':60,'cool':60}
-    print(picture(standar,data,'B0742025'))
-    print(return_message(data,'B0742025'))
+    standar={'知識_40%':80,'能力_40%':70,'態度_20%':60}
+    print(len(search_ID_DICT(data,'b0924017')['ID'])==0)
+    print(picture(standar,data,'b0924017'))
+    print(return_message(data,'b0924017'))
 
 
 
