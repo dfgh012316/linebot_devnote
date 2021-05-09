baca16b4ae51e0f59c716d4b6f25415aeb59e45d
Jerry Chuang <dfgh012316@gmail.com>
2021-05-09 15:19:03 +0800

    優化程式碼易讀性(flake8)
    讓程式碼符合flake8規範
    (ignore E501
    --max-line-length=140 )

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-from flask import Flask, request, abort,render_template
+from flask import Flask, request, abort
 import matplotlib.pyplot as plt
-import os,sys,sqlite3,json
+import os
 from dotenv import load_dotenv
 from linebot import (
     LineBotApi, WebhookHandler)
 from linebot.exceptions import (
     InvalidSignatureError)
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,StickerSendMessage,PostbackEvent,)
-from models.plot import picture,judge,return_pass_subject,create_data,search_ID_DICT,flex_grade
-import pandas as pd
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage)
+from models.plot import (
+  picture, judge, return_pass_subject, create_data, search_ID_DICT, flex_grade)
 
 app = Flask(__name__)
 
-#Enviroment Setting
+# Enviroment Setting
 load_dotenv()
-channel_secret=os.getenv('LINE_CHANNEL_SECRET')
-channel_access_token=os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
+plt.switch_backend('agg')
+plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta']
 
-plt.switch_backend('agg') #不需要圖形介面的的backend
-plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] #顯示中文字
+# 讀取成績單及通過標準
+data = create_data()
+standar = {'知識_40%': 80, '能力_40%': 70, '態度_20%': 60}
 
-#讀取成績單及通過標準
-data=create_data()
-standar={'知識_40%':80,'能力_40%':70,'態度_20%':60}
 
-#機器人
+# 機器人
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -35,13 +35,14 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    #app.logger.info("Request body: " + body) 會報錯先註解掉
+    # app.logger.info("Request body: " + body) 會報錯先註解掉
 
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
+        print("Invalid signature. \
+               Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -49,58 +50,27 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    try :
-        reply_arr=[]                           #回覆的訊息list
-        input_data=event.message.text.upper()  #大小寫都可搜尋
-        if len(search_ID_DICT(data,input_data)['ID']) != 0 :  #判斷該學號是否存在
-            grade_picture=picture(standar,data,input_data)     #生成圖表及回傳URL
-            pass_subject=judge(standar,data,input_data)         #產生通過的科目List
-            reply_arr.append(flex_grade(data,input_data,grade_picture))  #回覆成績資料
-            if pass_subject :
-                content=return_pass_subject(pass_subject)
+    try:
+        reply_arr = []
+        input_data = event.message.text.upper()
+        if len(search_ID_DICT(data, input_data)['ID']) != 0:
+            grade_picture = picture(standar, data, input_data)
+            pass_subject = judge(standar, data, input_data)
+            reply_arr.append(flex_grade(data, input_data, grade_picture))
+            if pass_subject:
+                content = return_pass_subject(pass_subject)
                 reply_arr.append(TextSendMessage(content))
                 reply_arr.append(StickerSendMessage(
                     package_id='11537',
-                    sticker_id='52002735')
-                )
+                    sticker_id='52002735'))
+            line_bot_api.reply_message(event.reply_token, reply_arr)
+        else:
             line_bot_api.reply_message(
-                event.reply_token,reply_arr)
-        else :
-            line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("查無此學號，請重新輸入。")
-        )
+                event.reply_token, TextSendMessage("查無此學號，請重新輸入。"))
+    except input_data.error:
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage("LineBot怪怪的請聯絡老師或助教"))
 
-    except :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("LineBot怪怪的請聯絡老師或助教")
-        )
 
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    if event.postback.data == '查詢成績' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("請輸入您的學號+安全碼(兩碼)")
-        )
-    elif event.postback.data == '重要公告' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("目前尚未有公告唷！")
-        )
-    elif event.postback.data == '課程簡報' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("https://drive.google.com/drive/folders/1nP_q78_ZGVspPW3FgKP4Uy1HZIz7Exjk?usp=sharing")
-        )
-    elif event.postback.data == '補充教材' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("https://drive.google.com/drive/folders/15yB50RwCJ-Qu8iz5PjpxrsDQmBrZyoin?usp=sharing")
-        )
-    elif event.postback.data == '課程規定' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("https://drive.google.com/drive/folders/1fOI8PBk8spp1D_d3CNtruz-c3XsNuXlV?usp=sharing")
-        )
-    elif event.postback.data == '常見QA' :
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("https://drive.google.com/drive/folders/1P6ulxiTASSq-h7VvwuisO-CPizDr_0za?usp=sharing")
-        )
-
-if __name__ == "__main__":  
+if __name__ == "__main__":
     app.run(port=1234)
--- models/plot.py
+++ models/plot.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-import pandas as  pd
+import pandas as pd
 import numpy as np
 from linebot.models import FlexSendMessage
-def flex_grade(data,input_data,url):
-    bubble=FlexSendMessage(
+
+
+def flex_grade(data, input_data, url):
+    bubble = FlexSendMessage(
         alt_text="你的成績",
-        contents=
-            {
+        contents={
               "type": "bubble",
               "size": "mega",
               "direction": "ltr",
@@ -16,238 +17,213 @@ def flex_grade(data,input_data,url):
                  "size": "full",
                  "aspectMode": "fit",
                  "action": {
-                 "type": "uri",
-                 "label": "action",
-                 "uri": url
-            }
-            },
-            "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-             {
-              "type": "text",
-              "text": "成績",
-              "weight": "bold",
-              "size": "xl"
-            },
-      {
-        "type": "box",
-        "layout": "vertical",
-        "margin": "lg",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "box",
-            "layout": "baseline",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "text",
-                "text": "知識",
-                "color": "#aaaaaa",
-                "size": "sm",
-                "flex": 1
-              },
-              {
-                "type": "text",
-                "text": str(search_ID_DICT(data,input_data)['知識_40%']).replace('[','').replace(']','').replace(".0",""),
-                "wrap": True,
-                "color": "#666666",
-                "size": "sm",
-                "flex": 1
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "text",
-                "text": "能力",
-                "color": "#aaaaaa",
-                "size": "sm",
-                "flex": 1
-              },
-              {
-                "type": "text",
-                "text": str(search_ID_DICT(data,input_data)['能力_40%']).replace('[','').replace(']','').replace(".0",""),
-                "wrap": True,
-                "color": "#666666",
-                "size": "sm",
-                "flex": 1
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "text",
-                "text": "態度",
-                "size": "sm",
-                "color": "#aaaaaa",
-                "flex": 1
-              },
-              {
-                "type": "text",
-                "text": str(search_ID_DICT(data,input_data)['態度_20%']).replace('[','').replace(']','').replace(".0",""),
-                "flex": 5,
-                "wrap": True,
-                "size": "sm",
-                "flex": 1
-              }
-            ],
-            "spacing": "sm"
-          }
-        ]
-      },
-      {
-        "type": "separator",
-        "color": "#aaaaaa",
-        "margin": "xxl"
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "baseline",
-    "contents": [
-      {
-        "type": "text",
-        "text": "總排名",
-        "size": "md"
-      },
-      {
-        "type": "text",
-        "text": str(search_ID_DICT(data,input_data)['總排名']).replace('[','').replace(']','').replace(".0",""),
-        "margin": "none",
-        "size": "sm",
-        "color": "#aaaaaa"
-      }
-    ]
-  },
-  "styles": {
-    "header": {
-      "separator": True
-    }
-  }
-}
-    )
+                  "type": "uri",
+                  "label": "action",
+                  "uri": url}},
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "成績",
+                    "weight": "bold",
+                    "size": "xl"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "知識",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": str(search_ID_DICT(data, input_data)['知識_40%']).replace('[', '').replace(']', '').replace(".0", ""),
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 1
+                          }]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "能力",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": str(search_ID_DICT(data, input_data)['能力_40%']).replace('[', '').replace(']', '').replace(".0", ""),
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 1
+                          }]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "態度",
+                            "size": "sm",
+                            "color": "#aaaaaa",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": str(search_ID_DICT(data, input_data)['態度_20%']).replace('[', '').replace(']', '').replace(".0", ""),
+                            "flex": 5,
+                            "wrap": True,
+                            "size": "sm",
+                          }],
+                        "spacing": "sm"}]},
+                  {
+                      "type": "separator",
+                      "color": "#aaaaaa",
+                      "margin": "xxl"}]},
+              "footer": {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "總排名",
+                    "size": "md"},
+                  {
+                    "type": "text",
+                    "text": str(search_ID_DICT(data, input_data)['總排名']).replace('[', '').replace(']', '').replace(".0", ""),
+                    "margin": "none",
+                    "size": "sm",
+                    "color": "#aaaaaa"}]},
+              "styles": {
+                "header": {
+                  "separator": True}}})
     return bubble
+
 
 def create_data():
     df = pd.DataFrame()
-    data = pd.read_excel("測試成績單.xlsx", sheet_name=None,engine='openpyxl')
-    sheet = pd.ExcelFile("測試成績單.xlsx",engine='openpyxl')
+    data = pd.read_excel("測試成績單.xlsx", sheet_name=None, engine='openpyxl')
+    sheet = pd.ExcelFile("測試成績單.xlsx", engine='openpyxl')
     for s_name in sheet.sheet_names:
         df = pd.concat([df, data.get(s_name)], ignore_index=False)
 
     return df
 
-def search_ID_DICT(data,ID):
+
+def search_ID_DICT(data, ID):
     try:
-        a=data.loc[data['ID']==ID]
-        data_dict=a.to_dict('list')
+        a = data.loc[data['ID'] == ID]
+        data_dict = a.to_dict('list')
         return data_dict
-    except :
+    except a.error:
         return False
 
-def judge(standar,data,ID):
-    df=data.loc[data['ID']==ID].fillna(0)
-    grade=[int(df[i]) for i in df.columns[2:5]]      #成績  
-    subject=[i for i in df.columns[2:5]]             #科目
-    student_dict=dict(zip(subject,grade))
-    pass_subject=[]
-    for i in subject :
-        if standar[i]<=student_dict[i] :
+
+def judge(standar, data, ID):
+    df = data.loc[data['ID'] == ID].fillna(0)
+    grade = [int(df[i]) for i in df.columns[2:5]]
+    subject = [i for i in df.columns[2:5]]
+    student_dict = dict(zip(subject, grade))
+    pass_subject = []
+    for i in subject:
+        if standar[i] <= student_dict[i]:
             pass_subject.append(i)
     return pass_subject
 
-def plot_judge(standar,data,ID):
-    df=data.loc[data['ID']==ID].fillna(0)
-    pass_list=[]
-    pass_subject_list=judge(standar,data,ID)
 
-    subjects=[i for i in df.columns[2:5]]
+def plot_judge(standar, data, ID):
+    df = data.loc[data['ID'] == ID].fillna(0)
+    pass_list = []
+    pass_subject_list = judge(standar, data, ID)
 
-    for subject in subjects :
-        if subject not in pass_subject_list :
-            pass_list.append((subject,0))
-        else :
-            pass_list.append((subject,1))
+    subjects = [i for i in df.columns[2:5]]
+
+    for subject in subjects:
+        if subject not in pass_subject_list:
+            pass_list.append((subject, 0))
+        else:
+            pass_list.append((subject, 1))
 
     return pass_list
 
-def picture(standar,data,ID):
-    if len(search_ID_DICT(data,ID)['ID']) == 0 :
-        return "查無此學號，請重新輸入。"
-    else :
-        df=data.loc[data['ID']==ID].fillna(0)            #處理尚未填入成績的空欄位
-        grade=[int(df[i]) for i in df.columns[2:5]]      #成績  
-        grade2=[int(df[i]) for i in df.columns[11:14]]
-        subject=[i for i in df.columns[2:5]]
-        x=np.arange(len(subject)) #取得科目數量
-        
-        width=0.35
-        fig,ax=plt.subplots()
-        rects1 = ax.bar(x +width/2, grade, width,color='#84C1FF')   #學生目前成績
-        rects2 = ax.bar(x - width/2, grade2, width,color='#BE77FF')  #學生自評
 
+def picture(standar, data, ID):
+    if len(search_ID_DICT(data, ID)['ID']) == 0:
+        return "查無此學號，請重新輸入。"
+    else:
+        df = data.loc[data['ID'] == ID].fillna(0)
+        grade = [int(df[i]) for i in df.columns[2:5]]
+        grade2 = [int(df[i]) for i in df.columns[11:14]]
+        subject = [i for i in df.columns[2:5]]
+        x = np.arange(len(subject))
+        width = 0.35
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(x + width/2, grade, width, color='#84C1FF')
+        rects2 = ax.bar(x - width/2, grade2, width, color='#BE77FF')
         ax.set_ylabel('成績')
         ax.set_title(ID)
         ax.set_xticks(x)
-        ax.set_xticklabels(subject) 
-        ax.set_ylim([0,115])
+        ax.set_xticklabels(subject)
+        ax.set_ylim([0, 115])
+
         def autolabel(rects):
-    
+
             for rect in rects:
                 height = rect.get_height()
                 ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom',
-                    size=20)
+                            xy=(rect.get_x() + rect.get_width() / 2, height),
+                            xytext=(0, 3),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='center', va='bottom',
+                            size=20)
         autolabel(rects1)
         autolabel(rects2)
-
         fig.tight_layout()
-
-        pass_list=plot_judge(standar,data,ID)
-        subject,flag=zip(*pass_list)
-
-        for i, bar in enumerate(rects1): 
-            if flag[i] == 0 : 
-                bar.set_color("#FF7575")      #不及格的顏色
-
+        pass_list = plot_judge(standar, data, ID)
+        subject, flag = zip(*pass_list)
+        for i, bar in enumerate(rects1):
+            if flag[i] == 0:
+                bar.set_color("#FF7575")
         fig.savefig('static//{}.png'.format(ID))
         plt.close()
-        return 'https://0d88cd745e28.ngrok.io//static//{}.png'.format(ID)  
+        return 'https://0d88cd745e28.ngrok.io//static//{}.png'.format(ID)
+
 
 def return_pass_subject(pass_subject):
-    content="恭喜"
-    for i,j in enumerate(pass_subject):
-        if i == len(pass_subject)-1 :
-            content+=j
-        else :
-            content=content+j+','
-    return (content+'通過').replace("_40%","").replace("_20%","")
+    content = "恭喜"
+    for i, j in enumerate(pass_subject):
+        if i == len(pass_subject)-1:
+            content += j
+        else:
+            content = content+j+','
+    return (content + '通過').replace("_40%", "").replace("_20%", "")
+
 
 if __name__ == '__main__':
-    plt.switch_backend('agg') #不需要圖形介面的的backend
-    plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta'] #顯示中文字
-    data=create_data()
-    standar={'知識_40%':80,'能力_40%':70,'態度_20%':60}
-    
-    print(picture(standar,data,'B0924031'))
-    print(flex_grade(data,'B0924031',picture(standar,data,'B0924031')))
-
-
-
-
-
-
+    plt.switch_backend('agg')
+    plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta']
+    data = create_data()
+    standar = {'知識_40%': 80, '能力_40%': 70, '態度_20%': 60}
+    print(picture(standar, data, 'B0924031'))
+    print(flex_grade(data, 'B0924031', picture(standar, data, 'B0924031')))
