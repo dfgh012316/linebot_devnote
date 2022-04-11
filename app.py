d3e5ad84149b85ba90c9f7179c9c16e0b12ec9d9
qq96932100 <qq9693210000@gmail.com>
2022-04-11 16:15:59 +0800

    FIX/修復Invalid reply token問題
    修復因reply token重複使用而導致的Invalid reply token問題。

--- app.py
+++ app.py
@@ -23,34 +23,17 @@ channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
-<<<<<<< Updated upstream
-ngrok_url = 'https://61d9-61-56-180-227.ngrok.io'
-=======
 ngrok_url = 'https://5367-2401-e180-8853-a55c-d119-adb6-a12-3089.ngrok.io'
->>>>>>> Stashed changes
 plt.switch_backend('agg') #不需要圖形介面的的backend
 plt.rcParams['font.sans-serif'] = ['TaipeiSansTCBeta-Regular'] #顯示中文字
 
 #讀取成績單及通過標準
-<<<<<<< Updated upstream
-#data = create_data()
-#print(data['ID'])   #test by QQ
-#print(data)
-=======
->>>>>>> Stashed changes
 standar = {'知識_40%':100,'能力_40%':70,'態度_20%':60}
 
 # 讀取google sheets
 scope = ['https://www.googleapis.com/auth/spreadsheets'] # 移出來讀一次就好，太耗效能 (成績有更動請重新啟動linebot)
 creds = Credentials.from_service_account_file("linear-outcome-339410-10f813b7e005.json", scopes=scope)
-<<<<<<< Updated upstream
-#sheet = gspread.authorize(creds).open_by_url('https://docs.google.com/spreadsheets/d/16EYLZIy5aOsCNXav9I3Oc-Av67R6lDK0uDfowvV4HNQ/edit#gid=0')
 sheet = gspread.authorize(creds).open_by_url('https://docs.google.com/spreadsheets/d/1ZBCj7Rno-c4WX2auIB7mESEm17dXBCylNs6I8nikIfs/edit#gid=752433555')
-#spreadsheet_key_path = '16EYLZIy5aOsCNXav9I3Oc-Av67R6lDK0uDfowvV4HNQ'
-#sheet2 = gspread.authorize(creds).open_by_key(spreadsheet_key_path).sheet1
-=======
-sheet = gspread.authorize(creds).open_by_url('https://docs.google.com/spreadsheets/d/1ZBCj7Rno-c4WX2auIB7mESEm17dXBCylNs6I8nikIfs/edit#gid=752433555')
->>>>>>> Stashed changes
 
 worksheet = sheet.get_worksheet(0)  
 data = pd.DataFrame(worksheet.get_all_values())
@@ -72,29 +55,10 @@ data_filter3 = data3.iloc[4:,:]
 worksheet4 = sheet.get_worksheet(4)  
 data4 = pd.DataFrame(worksheet4.get_all_values()) 
 data_filter4 = data4.iloc[4:,:]
-<<<<<<< Updated upstream
 
 filtered_data = pd.concat([data_filter, data_filter1, data_filter2, data_filter3, data_filter4]).fillna(0)
 filtered_data = filtered_data.replace('',0) # 成績單上不是數值型態
 filtered_data = filtered_data.replace('#DIV/0!',0)
-#print('looooooooooooooook!!!!!!!!!!!\n'+str(filtered_data)+'\n')
-#data = pd.DataFrame(worksheet)
-
-#new_data = sheet2.get_all_records(head=4, empty2zero=True)
-#print(new_data)
-
-#print('herrrrrrrrrrrrrrrrrrrrrrrrrrrrre')
-#new_data = pd.DataFrame(new_data)
-#data = pd.DataFrame(worksheet.row_values(3))
-
-#print('herrrrrrrrrrrrrrrrrrrrrrrrrrrrre')
-#print(new_data.iloc[0])
-=======
-
-filtered_data = pd.concat([data_filter, data_filter1, data_filter2, data_filter3, data_filter4]).fillna(0)
-filtered_data = filtered_data.replace('',0) # 成績單上不是數值型態
-filtered_data = filtered_data.replace('#DIV/0!',0)
->>>>>>> Stashed changes
 
 #建立SQLite資料庫
 conn = sqlite3.connect('studentID_userID.db', check_same_thread=False)
@@ -102,11 +66,6 @@ cursor = conn.cursor()
 #cursor.execute('CREATE TABLE StudentID_UserID(StudentID, UserID)')  #建立資料表
 #conn.commit()
 
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
 #機器人
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -115,11 +74,7 @@ def callback():
     
     # get request body as text
     body = request.get_data(as_text=True)
-<<<<<<< Updated upstream
-
-=======
     print(body)
->>>>>>> Stashed changes
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -133,11 +88,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-<<<<<<< Updated upstream
-
-=======
     print('MessageEvent:', event.reply_token)
->>>>>>> Stashed changes
     start = time.time() #計算運行時間
     
     input_data = event.message.text.upper()
@@ -150,11 +101,7 @@ def handle_message(event):
             i = 0
             if(len(row.iloc[0,0]) > 1):
                 i = 1
-<<<<<<< Updated upstream
-            print('i am hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee : ' +str(len(row.iloc[0,0])))
-=======
             #print('i am hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee : ' +str(len(row.iloc[0,0])))
->>>>>>> Stashed changes
             user_id = event.source.user_id
             print("user_id =", user_id)           
             name = row.iloc[0,3]
@@ -165,12 +112,6 @@ def handle_message(event):
             cursor.execute('select * from StudentID_UserID where UserID = ?', [user_id]) #執行SQL語法 (判斷UserID是否存在)
             conn.commit()
             result = cursor.fetchone() #紀錄是否存在 用這個判斷
-<<<<<<< Updated upstream
-            print(result)
-            print(filtered_data)
-=======
-
->>>>>>> Stashed changes
             if(result == None):                
                 account_bubble = flex_account(stu_id, user_id)
                 line_bot_api.reply_message(event.reply_token, account_bubble)
@@ -178,11 +119,7 @@ def handle_message(event):
                 cursor.execute('select * from StudentID_UserID where StudentID = ? AND UserID = ?', [stu_id, user_id]) #執行SQL語法 (判斷輸入學號是否match UserID)
                 conn.commit()
                 result = cursor.fetchone() #紀錄是否存在 用這個判斷
-<<<<<<< Updated upstream
-                #print(result)
-=======
                 
->>>>>>> Stashed changes
                 if(result == None):
                     line_bot_api.reply_message(event.reply_token, TextSendMessage('您輸入的學號已被其他帳號綁定，有任何疑問請詢問助教或老師!'))
                 else:
@@ -191,62 +128,6 @@ def handle_message(event):
                     CoreValues_subtotal = row.iloc[0,-2]
                     attitude_subtotal = row.iloc[0,-1]
                     picture(standar, filtered_data, stu_id) #產生圖表
-<<<<<<< Updated upstream
-                    print(filtered_data)
-                
-                    values = [float(knowledge_subtotal), float(CoreValues_subtotal), float(attitude_subtotal)]
-                    print(stu_id)
-                    url = ngrok_url + '//static//' + str(stu_id) +'.png'
-                    print(url)
-
-                    #reply_arr.append(flex_grade(url, values))
-                    report_card = flex_grade(url, values)
-                    reply_arr.append(report_card)           
-                    #line_bot_api.reply_message(event.reply_token, report_card)
-                    #line_bot_api.reply_message(event.reply_token, TextSendMessage('Hello'))
-                    line_bot_api.reply_message(event.reply_token, reply_arr)
-        
-        '''
-        if len(search_ID_DICT(data,input_data)['ID']) != 0 :  #判斷該學號是否存在
-            grade_picture=picture(standar,data,input_data)     #生成圖表及回傳URL
-            pass_subject=judge(standar,data,input_data)         #產生通過的科目List
-            reply_arr.append(flex_grade(data,input_data,grade_picture))  #回覆成績資料
-            if pass_subject :
-                content=return_pass_subject(pass_subject)
-                reply_arr.append(TextSendMessage(content))
-                reply_arr.append(StickerSendMessage(
-                    package_ids='11537',
-                    sticker_id='52002735')
-                )
-            line_bot_api.reply_message(
-                event.reply_token,reply_arr)
-        '''
-        print("The time used to execute this is given below")
-
-        end = time.time()
-
-        print(end - start)
-
-        #if re.search('作業',input_data) :
-        #    line_bot_api.reply_message(event.reply_token,TextSendMessage('https://euni.niu.edu.tw/'))
-        if re.search('查詢成績',input_data):
-            event.reply_token,TextSendMessage("請輸入您的學號")
-        if re.search('作業繳交',input_data):
-            choose_bubble = flex_choose()
-            line_bot_api.reply_message(event.reply_token, choose_bubble)
-        #if re.search('綁定帳號',input_data):
-        #    account_bubble = flex_account()
-        #    line_bot_api.reply_message(event.reply_token, account_bubble)
-        #if re.search('確定綁定',input_data):
-        #    reply_arr.append(TextSendMessage('進行綁定中... 綁定完成!'))
-        #    cursor.execute('INSERT INTO StudentID_UserID VALUES (?, ?)', (stu_id, user_id)) #將UserID和學號做綁定 並記錄在SQLite內
-        #    conn.commit()
-        #    sqlite3.SQLITE_UPDATE
-        else:
-            line_bot_api.reply_message(
-            event.reply_token,TextSendMessage("查無此學號，請重新輸入。")
-        )
-=======
                     values = [float(knowledge_subtotal), float(CoreValues_subtotal), float(attitude_subtotal)]
                     url = ngrok_url + '//static//' + str(stu_id) +'.png'
 
@@ -268,7 +149,6 @@ def handle_message(event):
         #     line_bot_api.reply_message(
         #     event.reply_token,TextSendMessage("查無此學號，請重新輸入。")
         # )
->>>>>>> Stashed changes
     except:
         line_bot_api.reply_message(
             event.reply_token,TextSendMessage("LineBot怪怪的，請聯絡老師或助教")
@@ -276,10 +156,7 @@ def handle_message(event):
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
-<<<<<<< Updated upstream
-=======
     print('PostbackEvent:', event.reply_token)
->>>>>>> Stashed changes
     edata = query_string.parse(event.postback.data)
     print(event.postback.data)
     print(edata)
@@ -294,12 +171,6 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage('進行綁定中... 綁定完成!'))
     elif edata['answer'] == 'no' :
         line_bot_api.reply_message(event.reply_token, TextSendMessage('取消綁定，請重新操作。'))
-<<<<<<< Updated upstream
-    #elif edata['action'] == '作業繳交' :
-    #    choose_bubble = flex_choose()
-    #    line_bot_api.reply_message(event.reply_token, choose_bubble)
-=======
->>>>>>> Stashed changes
     elif edata['action'] == '心理學a':
         action_bubble = flex_PSY_A()
         line_bot_api.reply_message(event.reply_token, action_bubble)
